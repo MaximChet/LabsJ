7862db48111dc5098cb8970f023c32f11e5c01a1
WasdTop <48046134+WasdTop@users.noreply.github.com>
2020-06-10 00:12:25 +0300

    Initial commit

---
+++ src/Lab1/Primes.java
@@ -0,0 +1,18 @@
+package Lab1;
+
+/**
+ * @author shaidullin
+ */
+
+public class Primes {
+    public static void main(String[] args) {
+        for (int m = 2; m < 100; m++)
+            if (isPrime(m)) System.out.print(m + ", ");
+    }
+    public static boolean isPrime(int n) {
+        for (int s = 2; s < n; s++)
+            if (n % s == 0)  return false;
+        return true;
+    }
+}
+
